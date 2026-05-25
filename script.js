00ad025d0b55605e53955bda544b2fa963fdf0b1
Ricardo Castro Xavier de Brito <ricardocastro7711@gmail.com>
2026-05-25 10:53:38 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ function receberQuantidadePessoas(evento){
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
@@ -39,6 +39,14 @@ function receberPorcentagemBotao(evento){
             botao.classList.add("botao-ativo")
         }
     })
+
+    if (evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
     
-    porcentagem = parseFloat(evento.target.value) / 100
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
